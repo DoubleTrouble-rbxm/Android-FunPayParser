9883b0c384951c76d276957f9944fb1e23c1918e
DoubleTrouble-rbxm <ldanilka18@gmail.com>
2025-09-19 14:59:38 +0300

    AndroidManifest added

--- app/src/main/java/com/example/funpayparser/ui/theme/Theme.kt
+++ app/src/main/java/com/example/funpayparser/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.funpayparser.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
